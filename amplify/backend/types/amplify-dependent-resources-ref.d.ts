fafdc17b0d7192c7b1e402adbbd30054b7c97f84
EC2 Default User <ec2-user@ip-172-31-80-28.ec2.internal>
2022-10-24 07:27:48 +0000

    amplify practice

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyforum42321015": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
